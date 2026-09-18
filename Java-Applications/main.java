b77b09d2b6934cacebec7bd6bc596107e694cf18
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-09-18 07:04:02 +0530

    new work

---
+++ Java-Applications/main.java
@@ -0,0 +1,31 @@
+import java.util.* ;
+public class main{
+    public static void main(String[] args){
+
+        String Student =" Kathirsan ";
+      String University =" Sabaragamuwa University";
+      String Course =" Software Engineering";
+     String Target=" WSO2";
+       System.out.println (Student);
+       System.out.println (University);
+       System.out.println (Course);
+       System.out.println (Target);
+           int marks = 75;
+           int sum =0;
+           
+           if(marks>=50){
+         System.out.println ("pass");
+         }
+         else{
+         System.out.println ("fail");
+        
+         }
+
+         for(int i =1;i<=10;i=i+1){
+            sum = sum+i;
+           System.out.println(sum);
+          System.out.println (i);
+         }
+
+    }
+}
